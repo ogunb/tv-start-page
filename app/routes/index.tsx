c96e9c396a660dafd152e9a61791aa4c81707a0d
Ogun Babacan <ogun.babacan@trendyol.com>
2022-01-24 00:36:22 +0300

    chore: basic warm up

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,20 @@
-import { Button } from '@mantine/core';
-import { ActionFunction } from 'remix';
+import { Button, Input } from '@mantine/core';
+import { ActionFunction, useActionData } from 'remix';
 
-export const action: ActionFunction = async (...args) => {
-  console.log(args);
-  return null;
+export const action: ActionFunction = async ({ request }) => {
+  const fields = Object.fromEntries(await request.formData());
+  return fields;
 };
 
 export default function Index() {
+  const actionData = useActionData();
+  console.log(actionData);
   return (
-    <form>
-      <Button type="submit">Submit</Button>
-    </form>
+    <>
+      <form method="post" action="/?index">
+        <Input name="input" type="email" />
+        <Button type="submit">Submit</Button>
+      </form>
+    </>
   );
 }
