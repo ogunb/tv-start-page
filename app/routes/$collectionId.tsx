e740dacae6e9c8016d321246c2122db2e18f0af4
Ogun Babacan <ogun.babacan@trendyol.com>
2022-01-30 00:21:00 +0300

    feat: redirect to collection selection with latest collection id

--- app/routes/$collectionId.tsx
+++ app/routes/$collectionId.tsx
@@ -1,4 +1,6 @@
 import {
+  ActionFunction,
+  Form,
   Link,
   LoaderFunction,
   redirect,
@@ -11,6 +13,7 @@ import { requireAccessToken } from '~/utils/session.server';
 import Input from '~/components/Input';
 import { ShowCard } from '~/components/ShowCard';
 import Button from '~/components/Button';
+import { collectionIdCookie } from '../utils/raindrop.server';
 
 type LoaderData = {
   shows: Show[];
@@ -50,12 +53,39 @@ export const loader: LoaderFunction = async ({
   };
 };
 
+const redirectToCollectionSelection = async (collectionId?: string) => {
+  return redirect(`/?latest=${collectionId}`, {
+    headers: {
+      'Set-Cookie': await collectionIdCookie.serialize({
+        collectionId: null,
+      }),
+    },
+  });
+};
+
+export const action: ActionFunction = ({ request, params }) => {
+  switch (request.method) {
+    case 'DELETE': {
+      return redirectToCollectionSelection(params.collectionId);
+      break;
+    }
+    default: {
+      throw new Response('Method not Allowed', { status: 405 });
+    }
+  }
+
+  return null;
+};
+
 export default function Collection() {
   const { shows, showCount } = useLoaderData<LoaderData>();
 
   return (
     <div className="py-5 xl:px-20">
       <div className="flex flex-col mx-auto max-w-xl mb-5">
+        <Form method="delete">
+          <Button className="w-full mb-5">Change Collection</Button>
+        </Form>
         <Input placeholder="Filter by name..." />
         <p>Found total of {showCount} items in your collection.</p>
       </div>
@@ -73,8 +103,8 @@ export function CatchBoundary() {
   const caught = useCatch();
 
   return (
-    <div>
-      <h2>{caught.data}</h2>
+    <div className="max-w-3xl h-full flex flex-col items-center justify-center mx-auto text-center">
+      <h2 className="mb-5">{caught.data}</h2>
       <Link to="/">
         <Button>Lets retry, shall we?</Button>
       </Link>
